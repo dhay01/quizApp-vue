c107a9e3fbc4ab5bab8943b997dac4b028633cf2
dhay salih <dhaysalih18@gmail.com>
2024-04-30 13:04:12 +0300

    first commit

---
+++ src/components/Questions.vue
@@ -0,0 +1,28 @@
+<template>
+
+  <div class="single-question" v-for="(question, qi) in questions" :key="question.q"
+       v-show="questionsAnswered==qi">
+    <div class="question">{{ question.q }}</div>
+    <div class="answers" v-for="answer in question.answers" :key="answer.text" @click.prevent="selectAnswer(answer.is_correct)">
+      <div class="answer">{{ answer.text }}</div>
+
+
+    </div>
+  </div>
+
+</template>
+<script>
+export default {
+  props: ['questions', 'questionsAnswered'],
+  emits: ["answered"],
+  methods:{
+    selectAnswer(is_correct) {
+        this.$emit('answered')
+    }
+
+  }
+};
+</script>
+<style>
+
+</style>
